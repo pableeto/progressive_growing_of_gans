46c4715a867e27c64fec54ff93ef462ea99043d7
v-xil <v-xil@microsoft.com>
2018-12-02 17:01:48 +0800

    fix

--- dataset_tool.py
+++ dataset_tool.py
@@ -633,7 +633,16 @@ def create_from_adobe_images(tfrecord_dir, image_dir, shuffle):
     print(image_filenames[0])
     img = np.asarray(PIL.Image.open(image_filenames[0]))
     cv2.imwrite('/mnt/pgan_experiments/test_full.jpg', img[...,::-1])
+    img_1 = np.concatenate([
+        img[:, resolution:2*resolution], 
+        img[:, 3*resolution:4*resolution,0:1],
+        img[:, 2*resolution:3*resolution]
+    ], axis = -1)
+    cv2.imwrite('/mnt/pgan_experiments/test_0.jpg', img_1[...,0:3][...,::-1])
+    cv2.imwrite('/mnt/pgan_experiments/test_1.jpg', img_1[...,3][...,::-1])
+    cv2.imwrite('/mnt/pgan_experiments/test_2.jpg', img_1[...,4:7][...,::-1])    
 
+    input()
     resolution = img.shape[0]
     channels = 7 #if img.ndim == 3 else 1
 
